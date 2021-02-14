7774cf6c41e3ab054247bdaf7088fc75790c0ea6
Błażej Drozd <59662074+Tsugumik@users.noreply.github.com>
2021-02-14 16:33:06 +0100

    Memory optimization

--- code.cpp
+++ code.cpp
@@ -12,8 +12,8 @@ struct rect
 };
 
 // Prototypy
-polar rect_to_polar(rect xypos);
-void show_polar(polar dapos);
+void rect_to_polar(const rect * pxy, polar * pda);
+void show_polar(const polar * pda);
 
 int main()
 {
@@ -23,8 +23,8 @@ int main()
     cout << "Podaj x i y: ";
     while (cin >> rplace.x >> rplace.y)
     {
-        pplace = rect_to_polar(rplace);
-        show_polar(pplace);
+        rect_to_polar(&rplace, &pplace);
+        show_polar(&pplace);
         cout << "Podaj nastepne liczby lub q, aby wyjsc: ";
     }
     cout << "Koniec\n";
@@ -32,22 +32,20 @@ int main()
 }
 
 // Zamiana współrzędnych prostokątnych na biegunowe
-polar rect_to_polar(rect xypos)
+void rect_to_polar(const rect * pxy, polar * pda)
 {
     using namespace std;
-    polar answer;
-    answer.distance =
-        sqrt(xypos.x * xypos.x + xypos.y * xypos.y);
-    answer.angle = atan2(xypos.y, xypos.x);
-    return answer;
+    pda->distance =
+        sqrt(pxy->x * pxy->x + pxy->y * pxy->y);
+    pda->angle = atan2(pxy->y, pxy->x);
 }
 
 // pokazuje współrzędne biegunowe, radiany przelicza na stopnie
-void show_polar(polar dapos)
+void show_polar(const polar * pda)
 {
     using namespace std;
     const double rad_to_deg = 57.29577951;
 
-    cout << "Odleglosc = " << dapos.distance << endl;
-    cout << "Kat = " << dapos.angle * rad_to_deg << " stopni\n";
+    cout << "Odleglosc = " << pda->distance << endl;
+    cout << "Kat = " << pda->angle * rad_to_deg << " stopni\n";
 }
